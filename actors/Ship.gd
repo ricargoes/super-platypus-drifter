7ebb88c6e4a6c909e64a5a9513b35568332481c0
Ricardo <ricardogomezescalante@gmail.com>
2020-10-03 15:21:49 +0200

    Planet locking

--- actors/Ship.gd
+++ actors/Ship.gd
@@ -3,14 +3,17 @@ extends KinematicBody2D
 const ACCELERATION = 30
 const TURN_SPEED = 5
 var speed = Vector2(0, 0)
+var is_lock = false
 
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
 	pilot(delta)
-	var kinematic_collision = move_and_collide(speed*delta)
+	if is_lock:
+		return
 	
+	var kinematic_collision = move_and_collide(speed*delta)
 	if kinematic_collision:
 		die()
 
@@ -23,11 +26,30 @@ func pilot(delta):
 		$BoosterAnim.show()
 		$BoosterAnim.play()
 		speed += ACCELERATION*delta*Vector2(cos(rotation), sin(rotation))
+		$UnlockedTime.start()
+		is_lock = false
 	else:
 		$BoosterAnim.hide()
 	
-	if Input.is_action_pressed("ship_turn_left"):
-		rotation -= TURN_SPEED*delta
-	elif Input.is_action_pressed("ship_turn_right"):
-		rotation += TURN_SPEED*delta
+	if not is_lock:
+		if Input.is_action_pressed("ship_turn_left"):
+			rotation -= TURN_SPEED*delta
+		elif Input.is_action_pressed("ship_turn_right"):
+			rotation += TURN_SPEED*delta
 
+func orienting_to(target_angle, delta, tolerance = 0.1):
+	var angle_diff = fposmod(get_rotation()-target_angle,2*PI)
+	if (abs(angle_diff) < tolerance):
+		return true
+	else:
+		var strength = sqrt(1 - abs(angle_diff/PI - 1))
+		if( angle_diff >= 0 and abs(angle_diff) <= PI ) or \
+			( angle_diff <= 0 and abs(angle_diff) >= PI ):
+			rotation -= TURN_SPEED*delta
+		elif( angle_diff <= 0 and abs(angle_diff) <= PI ) or \
+			( angle_diff >= 0 and abs(angle_diff) >= PI ):
+			rotation += TURN_SPEED*delta
+		return false
+
+func is_unrestricted():
+	return not $UnlockedTime.is_stopped()
---
+++ planets/Planet.gd
@@ -0,0 +1,30 @@
+extends StaticBody2D
+
+var orbit_speed = PI/10
+const CATCHING_SPEED = 30
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	var ships = $Orbit.get_overlapping_bodies()
+	for ship in ships:
+		print(ship.speed.length())
+		print(ship.is_lock)
+		if ship.is_lock:
+			var ship_from_planet = ship.position - position
+			var r = ship_from_planet.length()
+			var angle = ship_from_planet.angle()
+			
+			ship.position = position + r*Vector2(cos(angle+orbit_speed*delta), sin(angle+orbit_speed*delta))
+			ship.orienting_to(ship_from_planet.angle()+ PI/2, delta)
+			ship.speed = Vector2(cos(ship.rotation), sin(ship.rotation))*CATCHING_SPEED
+		elif not ship.is_unrestricted():
+			if ship.speed.length() < CATCHING_SPEED:
+				ship.is_lock = true
+			else:
+				ship.speed = ship.speed * 0.1
+
+
+func _on_Orbit_body_exited(body):
+	body.is_lock = false
