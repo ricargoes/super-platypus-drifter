0aa5ebaa919b45d8a309c0e9d9ca9a74067d069e
Ricardo <ricardogomezescalante@gmail.com>
2020-10-03 19:02:33 +0200

    Boost now charges firts and then accelerate suddenly

--- actors/Ship.gd
+++ actors/Ship.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
-const ACCELERATION = 30
+const ACCELERATION = 10
 const TURN_SPEED = 5
+const MAX_BOOST_CHARGE = 100
+const BOOST_CHARGE_RATE = 30
 var speed = Vector2(0, 0)
 var is_lock = false
+var boost_charge = 0
 
 func _ready():
 	set_physics_process(true)
@@ -20,15 +23,15 @@ func _physics_process(delta):
 func die():
 	queue_free()
 
-
 func pilot(delta):
 	if Input.is_action_pressed("ship_boost"):
 		$BoosterAnim.show()
 		$BoosterAnim.play()
-		speed += ACCELERATION*delta*Vector2(cos(rotation), sin(rotation))
+		boost_charge = min(boost_charge + BOOST_CHARGE_RATE*delta, MAX_BOOST_CHARGE)
 		$UnlockedTime.start()
 		is_lock = false
 	else:
+		boost()
 		$BoosterAnim.hide()
 	
 	if not is_lock:
@@ -36,6 +39,10 @@ func pilot(delta):
 			rotation -= TURN_SPEED*delta
 		elif Input.is_action_pressed("ship_turn_right"):
 			rotation += TURN_SPEED*delta
+
+func boost():
+	speed += ACCELERATION*boost_charge*Vector2(cos(rotation), sin(rotation))
+	boost_charge = 0
 
 func orienting_to(target_angle, delta, tolerance = 0.1):
 	var angle_diff = fposmod(get_rotation()-target_angle,2*PI)
