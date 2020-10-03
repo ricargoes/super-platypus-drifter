211453140118829f43d8106570ee9da9cfa56c15
Ricardo <ricardogomezescalante@gmail.com>
2020-10-03 22:02:22 +0200

    different textures for planets

---
+++ planets/StablePlanet.gd
@@ -0,0 +1,20 @@
+extends "res://planets/Planet.gd"
+
+var textures = [
+	preload("res://planets/Planet 1.png"), 
+	preload("res://planets/Planet2.png"), 
+	preload("res://planets/planet3.png")
+]
+
+export var random_texture = true
+export var texture_number = 0
+
+func _ready():
+	var index
+	if random_texture:
+		seed("tutupa".hash())
+		index = randi() % textures.size()
+	else:
+		index = texture_number
+		
+	$Sprite.texture = textures[index]
