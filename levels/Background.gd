664bfd5fe3a56e3df90f0870f85a175ae002b3a2
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-10-04 20:14:53 +0200

    Fix background

--- levels/Background.gd
+++ levels/Background.gd
@@ -3,7 +3,6 @@ extends Sprite
 func _ready():
 	var root = get_node(".")
 	root.set_region_rect(Rect2(
-		-SPD.LEVEL_WIDTH, 0,
-		SPD.MAX_SCREENS * SPD.LEVEL_WIDTH, SPD.LEVEL_HEIGHT)
+		0, 0, SPD.MAX_SCREENS * SPD.LEVEL_WIDTH, SPD.LEVEL_HEIGHT)
 	)
 	root.global_translate(Vector2(-SPD.LEVEL_WIDTH/2, 0))
