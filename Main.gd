f5f467b4e68bbb5e395c3c2a18cb26eb7097eefc
Ricardo <ricardogomezescalante@gmail.com>
2020-10-04 23:17:56 +0200

    Main menu

--- Main.gd
+++ Main.gd
@@ -1,4 +1,17 @@
 extends Control
 
-func _ready():
+
+func _on_Tutorial_pressed():
+	pass
+
+
+func _on_Endless_pressed():
+	var _unused = get_tree().change_scene("res://levels/ProceduralLevel.tscn")
+
+
+func _on_Story_pressed():
 	var _unused = get_tree().change_scene("res://levels/Level1.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
