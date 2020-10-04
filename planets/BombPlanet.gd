a19d425486e73a03f6d3f1839059112b00a0f9de
Ricardo <ricardogomezescalante@gmail.com>
2020-10-04 14:34:01 +0200

    bug: BombPlanet is freed after explosion

--- planets/BombPlanet.gd
+++ planets/BombPlanet.gd
@@ -3,7 +3,7 @@ extends "res://planets/Planet.gd"
 export var countdown = 5.0
 
 func _ready():
-	$BombAnim.speed_scale = 1/countdown
+	$BombAnim.speed_scale = 1.0/countdown
 
 func _on_Orbit_body_entered(body):
 	if not $BombAnim.playing: $BombAnim.play()
@@ -17,5 +17,4 @@ func _on_BombAnim_animation_finished():
 	$ExplosionAnim.play()
 
 func _on_ExplosionAnim_animation_finished():
-	$ExplosionAnim.stop()
-	$ExplosionAnim.hide()
+	queue_free()
