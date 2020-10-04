5e833c640b7f7a95a00f9d3f5e8cbd89217f49b0
Ricardo <ricardogomezescalante@gmail.com>
2020-10-04 11:49:40 +0200

    The fearsome BombPlanet is on

---
+++ planets/BombPlanet.gd
@@ -0,0 +1,21 @@
+extends "res://planets/Planet.gd"
+
+export var countdown = 5.0
+
+func _ready():
+	$BombAnim.speed_scale = 1/countdown
+
+func _on_Orbit_body_entered(body):
+	if not $BombAnim.playing: $BombAnim.play()
+
+func _on_BombAnim_animation_finished():
+	$BombAnim.hide()
+	$ExplosionAnim.show()
+	var bodies = $Orbit.get_overlapping_bodies()
+	for body in bodies:
+		body.die()
+	$ExplosionAnim.play()
+
+func _on_ExplosionAnim_animation_finished():
+	$ExplosionAnim.stop()
+	$ExplosionAnim.hide()
