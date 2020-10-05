01faac6bef146982463c645e4a596b6107fd0e54
Ricardo <ricardogomezescalante@gmail.com>
2020-10-06 00:11:15 +0200

    fix: crashing bug

--- levels/Level.gd
+++ levels/Level.gd
@@ -14,15 +14,15 @@ func _ready():
 	camera.position = camera_start.position
 
 	# Initial position for the ship
-	ship = SPD.SHIP_SCENE.instance()
-	ship.position = ship_start.position
-	if not do_witty_comments: ship.get_node("Bubble/WittyComment").queue_free()
-	add_child(ship)
+	ship = weakref(SPD.SHIP_SCENE.instance())
+	ship.get_ref().position = ship_start.position
+	if not do_witty_comments: ship.get_ref().get_node("Bubble/WittyComment").queue_free()
+	add_child(ship.get_ref())
 	set_physics_process(true)
 
 func _physics_process(delta):
 	camera.global_translate(Vector2(scroll_speed * delta, 0))
-	if ship:
+	if ship and ship.get_ref():
 		_update_game_state()
 	else:
 		SPD.journey_length = int(camera.position.x/100)
@@ -31,11 +31,11 @@ func _physics_process(delta):
 func _update_game_state():
 
 	# Ship killed by level limits
-	var ship_camera_displacement = ship.position.x - camera.position.x
+	var ship_camera_displacement = ship.get_ref().position.x - camera.position.x
 	if ship_camera_displacement < -SPD.LEVEL_WIDTH/2:
-		ship.die()
+		ship.get_ref().die()
 	if ship_camera_displacement > SPD.LEVEL_WIDTH/2:
-		ship.die()
+		ship.get_ref().die()
 
 
 func _on_Endline_body_entered(body):
