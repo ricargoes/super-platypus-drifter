db15056a1c19e0299cb5d8137338cc7ad11073c2
Ricardo <ricardogomezescalante@gmail.com>
2020-10-05 15:01:20 +0200

    bug: previous level is freed before charging next one

--- levels/Supervisor.gd
+++ levels/Supervisor.gd
@@ -8,14 +8,16 @@ var levels_scene = [
 	preload("res://levels/Level1.tscn"),
 	preload("res://levels/Level1.tscn")
 ]
+var current_levels_scene = null
 
 func _ready():
 	load_level()
 
 func load_level():
-	var level_scene = levels_scene[level-1].instance()
-	add_child(level_scene)
-	level_scene.connect("level_ended", self, "next_level")
+	if current_levels_scene: current_levels_scene.queue_free()
+	current_levels_scene = levels_scene[level-1].instance()
+	add_child(current_levels_scene)
+	current_levels_scene.connect("level_ended", self, "next_level")
 
 func next_level():
 	level += 1
